7d020e8dfb649b58bfa37524d9ffa0abf41e8105
Sean <wasdwasd0105@gmail.com>
2026-01-21 05:15:43 +0800

    rewrite

--- system/android_entry.c
+++ system/android_entry.c
@@ -8,15 +8,36 @@
 #include <fcntl.h>
 #include <android/log.h>
 
-// #define LOG_TAG "QEMU"
-// #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-// #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOG_TAG "Lib QEMU"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-extern int qemu_main(int argc, char **argv);
+#include "qemu/osdep.h"
+#include "qemu-main.h"
+#include "qemu/main-loop.h"
+#include "qemu/thread.h"
+#include "system/replay.h"
+#include "system/system.h"
+
+extern int (*qemu_main)(void);
 
 typedef struct {
     int fd;
 } LogThreadArgs;
+
+static void *qemu_default_main(void *opaque)
+{
+    int status;
+
+    replay_mutex_lock();
+    bql_lock();
+    status = qemu_main_loop();
+    qemu_cleanup(status);
+    bql_unlock();
+    replay_mutex_unlock();
+
+    exit(status);
+}
 
 static void *logcat_thread_fn(void *arg) {
     LogThreadArgs *a = (LogThreadArgs *)arg;
@@ -31,7 +52,7 @@ static void *logcat_thread_fn(void *arg) {
         n = read(fd, buf, sizeof(buf) - 1);
         if (n > 0) {
             buf[n] = 0;
-            //__android_log_write(ANDROID_LOG_INFO, LOG_TAG, buf);
+            __android_log_write(ANDROID_LOG_INFO, LOG_TAG, buf);
         } else {
             // non-blocking pipe: no data or temporary condition
             usleep(10 * 1000); // 10ms
@@ -59,10 +80,24 @@ int android_qemu_start(int argc, char **argv) {
         pthread_detach(log_thread);
     }
 
-    //LOGI("Starting QEMU...");
-    int ret = qemu_main(argc, argv);
-    //LOGI("QEMU exited with code %d", ret);
-    return ret;
+    LOGI("Starting QEMU...");
+
+    qemu_init(argc, argv);
+    bql_unlock();
+    replay_mutex_unlock();
+
+    if (qemu_main) {
+        QemuThread main_loop_thread;
+        qemu_thread_create(&main_loop_thread, "qemu_main",
+                           qemu_default_main, NULL, QEMU_THREAD_DETACHED);
+        int ret = qemu_main();
+        LOGI("QEMU exited with code %d", ret);
+        return ret;
+    } else {
+        /* Never returns (calls exit). */
+        qemu_default_main(NULL);
+        __builtin_unreachable();
+    }
 }
 
 typedef struct {
@@ -85,10 +120,11 @@ Java_com_vectras_qemu_jni_Loader_startQemu(JNIEnv *env, jclass clazz, jobjectArr
     (void)clazz;
 
     int argc = (*env)->GetArrayLength(env, jargs);
+    LOGI("Jni Starting QEMU...");
 
     QemuArgs *qa = (QemuArgs *)malloc(sizeof(QemuArgs));
     qa->argc = argc;
-    qa->argv = (char **)malloc(sizeof(char*) * argc);
+    qa->argv = (char **)malloc(sizeof(char*) * (argc + 1));
 
     for (int i = 0; i < argc; i++) {
         jstring js = (jstring)(*env)->GetObjectArrayElement(env, jargs, i);
@@ -97,6 +133,7 @@ Java_com_vectras_qemu_jni_Loader_startQemu(JNIEnv *env, jclass clazz, jobjectArr
         (*env)->ReleaseStringUTFChars(env, js, str);
         (*env)->DeleteLocalRef(env, js);
     }
+    qa->argv[argc] = NULL;
 
     pthread_t thread;
     pthread_create(&thread, NULL, qemu_thread_fn, qa);
